3d9ca0d2c82af884265dee7358fae7cdd17a0fb7
古俊杰 <ilovintit@gmail.com>
2020-01-11 01:13:19 +0800

    修正版本升级的bug

--- src/RegionServiceProvider.php
+++ src/RegionServiceProvider.php
@@ -3,35 +3,36 @@
 namespace ZhiEq\Region;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class RegionServiceProvider extends ServiceProvider
 {
-    protected $defer = true;
+  protected $defer = true;
 
-    protected $validators = [
-        'region_exists',
-        'region_level',
-        'region_province',
-        'region_province_exists',
-        'region_city',
-        'region_city_exists',
-        'region_county',
-        'region_county_exists'
-    ];
+  protected $validators = [
+    'region_exists',
+    'region_level',
+    'region_province',
+    'region_province_exists',
+    'region_city',
+    'region_city_exists',
+    'region_county',
+    'region_county_exists'
+  ];
 
-    /**
-     * @return \Illuminate\Validation\Factory
-     */
+  /**
+   * @return \Illuminate\Validation\Factory
+   */
 
-    protected function validator()
-    {
-        return $this->app['validator'];
+  protected function validator()
+  {
+    return $this->app['validator'];
+  }
+
+  public function boot()
+  {
+    foreach ($this->validators as $validator) {
+      $this->validator()->extend($validator, 'ZhiEq\Region\Validators\\' . Str::studly($validator) . 'Validator@validator');
     }
-
-    public function boot()
-    {
-        foreach ($this->validators as $validator) {
-            $this->validator()->extend($validator, 'ZhiEq\Region\Validators\\' . studly_case($validator) . 'Validator@validator');
-        }
-    }
+  }
 }
